4d32edb1abf0fcb5bcb354a8ba85c599aaa25d2e
gagandeepattri <gagandeep.attri@gmail.com>
2025-12-08 14:31:06 +0000

    print

--- src/main/java/sample/taas/java/DemoApplication.java
+++ src/main/java/sample/taas/java/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.print("Hello to first Taas demo project");
 	}
 
 }
